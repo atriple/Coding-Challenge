432dc30fe455b609019d9f926e13c498a48b61ac
atriple <andiaqil.dpe@gmail.com>
2019-04-02 19:12:31 +0700

    Added v.1.0

--- Auto-Group-Assigner/README.md
+++ Auto-Group-Assigner/README.md
@@ -3,3 +3,8 @@ Allocate **uniformly** certain numbers of people to certain numbers of group.
 
 ## Partitioning
 Currently I have to do it manually, but I will try to create it on the next version.
+
+
+## TODO :
+- [ ] Improvise Algorithm. I think there's much more efficient way to do this...
+- [ ] There must be more shorter/simpler way to write this...
---
+++ Auto-Group-Assigner/group-assign.py
@@ -0,0 +1,39 @@
+from random import shuffle
+import csv
+
+results = []
+with open("test.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader: # each row is a list
+        results.append(row)
+
+listLength = len(results)
+print("Total member(s) : " + str(listLength))
+numberOfGroup = int(input("Jumlah grup : "))
+
+# Lower Bound and Upper Bound to distribute members.
+try :
+    DIFF = int(listLength/numberOfGroup)
+except :
+    print("ERROR : INVALID VALUE !")
+else :
+    if DIFF < 1:
+        print("Tidak bisa memproses value ini, kemungkinan jumlah grup lebih dari total member")
+        raise ValueError('ERROR VALUE')
+finally :
+    low = 0
+    up = DIFF
+    print (up) #Hanya sekedar debug.
+    print (low) #Hanya sekedar debug.
+
+shuffle(results) #shuffle/randomize list sebelum di alokasi
+
+x = numberOfGroup
+while True:
+    group = results[low:up]
+    low = low + DIFF
+    up = up + DIFF
+    print(group)
+    x = x - 1
+    if x == 0:
+        break

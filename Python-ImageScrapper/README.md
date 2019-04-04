d00647a54be4fa5f895151f79dcd76ae40cc2c73
atriple <andiaqil.dpe@gmail.com>
2019-04-04 11:43:12 +0700

    Update imagescrapper

---
+++ Python-ImageScrapper/README.md
@@ -0,0 +1,7 @@
+# Python Image Scrapper
+
+## Requirements :
+- Beautifulsoup4 (4.7.1)
+
+## TODO :
+- [ ] Interactive Command Line Options
--- Python-ImageScrapper/imagescrapper.py
+++ Python-ImageScrapper/imagescrapper.py
@@ -1,3 +1,25 @@
+from bs4 import BeautifulSoup
+import urllib
 import urllib.request
 
-urllib.request.urlretrieve("http://93.190.142.23/manga/Yancha-Gal-No-Anjou-san/0004-002.png", "test.png")
+# https://www.youtube.com/watch?v=m_agcM_ds1c
+
+def make_soup(url):
+    page = urllib.request.urlopen(url)
+    soupdata = BeautifulSoup(   page, 'html.parser')
+    return soupdata
+
+soup = make_soup("https://www.phd.co.id/en/pizza")
+
+for img in soup.findAll('img'):
+    temp = img.get('src')
+    print(temp)
+    '''
+    if temp[:1] == "/":
+        image = "https://www.phd.co.id/en/pizza" + temp
+    else : 
+        image = temp
+    
+    print (image)
+    '''
+

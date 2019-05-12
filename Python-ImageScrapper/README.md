1941616e795a83c4d567bb030e60a6bb17c1ed26
atriple <andiaqil.dpe@gmail.com>
2019-05-12 09:31:56 +0700

    Update Imagescrapper

--- Python-ImageScrapper/README.md
+++ Python-ImageScrapper/README.md
@@ -1,7 +1,12 @@
 # Python Image Scrapper
 
-## Requirements :
+## Requirements 
 - Beautifulsoup4 (4.7.1)
 
-## TODO :
+## TODO 
 - [ ] Interactive Command Line Options
+
+## Notes 
+https://www.youtube.com/watch?v=m_agcM_ds1c
+### Request Forbidden
+https://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping
--- Python-ImageScrapper/imagescrapper.py
+++ Python-ImageScrapper/imagescrapper.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-import urllib
 import urllib.request
 
 # https://www.youtube.com/watch?v=m_agcM_ds1c
@@ -8,20 +7,22 @@ def make_soup(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     page = urllib.request.urlopen(url)
     soupdata = BeautifulSoup(page, 'html.parser')
+    print(page)
     return soupdata
 
 soup = make_soup("https://www.phd.co.id/en/pizza")
 
+'''
 for img in soup.findAll('img'):
     print(img)
     temp = img.get('src')
     # print(temp)
-    '''
+    
     if temp[:1] == "/":
         image = "https://www.phd.co.id/en/pizza" + temp
     else : 
         image = temp
     
     print (image)
-    '''
-
+    
+'''

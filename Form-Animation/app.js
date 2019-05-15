2f5619848dac128840bbaa17fcffba23732842f6
atriple <andiaqil.dpe@gmail.com>
2019-05-15 15:16:47 +0700

    Add Form Animation project

---
+++ Form-Animation/app.js
@@ -0,0 +1,47 @@
+function animateForm() {
+    const arrowBtn = document.querySelectorAll(".fa-arrow-right");
+    console.log(arrowBtn);
+
+    arrowBtn.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const currInput = arrow.previousElementSibling;
+            const currForm = arrow.parentElement;
+            const nextForm = currForm.nextElementSibling;
+
+            //Validation Check
+            if (currInput.type === "text" && validateUser(currInput)){
+                console.log(currInput.value);
+                activateForm(currForm, nextForm);
+            }
+        });
+    });
+}
+
+function validateUser(user) {
+    if (user.value.length < 6) {
+        error("crimson");
+        console.log("Need more characters");
+    } 
+    else {
+        error("rgb(87,189,130)");
+        return true;
+    }
+}
+
+function activateForm(current, next){
+    /*
+    args : 
+    current --- the current form that want to be deactivated
+    next -- next form that will be activated
+    */
+   current.classList.add("inactive");
+   current.classList.remove("active");
+   next.classList.remove("inactive");
+   next.classList.add("active");
+}
+
+function error(color) {
+    document.body.style.backgroundColor = color;
+}
+
+animateForm();
